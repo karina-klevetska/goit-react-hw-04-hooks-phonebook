0902697d0476023577c62585b93a7596731da4dd
Karina Klevetska <karina.klevetska@gmail.com>
2021-11-04 13:46:58 +0200

    add filter id

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,18 +1,26 @@
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import './Filter.css'
 
-const Filter = ({ filterId, handleChangeFilter }) => {
-    return (
-        <div className="filter-container">
-            <label className="label" htmlFor={filterId}>Find contacts by name</label>
-            <input className="input" id={filterId} type="text" name="filter" onChange={handleChangeFilter} />
-        </div>
-    )
+const Filter = ({ handleChangeFilter }) => {
+  return (
+    <div className='filter-container'>
+      <label className='label' htmlFor='filter'>
+        Find contacts by name
+      </label>
+      <input
+        className='input'
+        id='filter'
+        type='text'
+        name='filter'
+        onChange={handleChangeFilter}
+      />
+    </div>
+  )
 }
 
 Filter.propTypes = {
-    filterId: PropTypes.string,
-    handleChangeFilter: PropTypes.func
+  filterId: PropTypes.string,
+  handleChangeFilter: PropTypes.func,
 }
 
 export default Filter
